1466c489a3d9a13df6c9762709fe610bd6c3284b
Max <mczonk@gmail.com>
2014-07-01 22:34:10 +0200

    Add Mac compatibility.

--- CoreVideoPlus.h
+++ CoreVideoPlus.h
@@ -1,5 +1,32 @@
 
-#import <CoreVideoPlus/CVPOpenGLESTexture.h>
-#import <CoreVideoPlus/CVPOpenGLESTextureCache.h>
+#include <TargetConditionals.h>
 
-#import <CoreVideoPlus/NSError+CVPError.h>
+
+#include "GLPPixelformat+CVPPixelformat.h"
+
+#ifdef __OBJC__
+
+#import "NSError+CVPError.h"
+
+#endif
+
+
+#if defined(TARGET_OS_MAC) && (TARGET_OS_MAC > 0)
+
+#ifdef __OBJC__
+
+#endif  /* __OBJC__ */
+
+#elif defined(TARGET_OS_IPHONE) && (TARGET_OS_IPHONE > 0)
+
+
+#ifdef __OBJC__
+
+#import "CVPOpenGLESTexture.h"
+#import "CVPOpenGLESTextureCache.h"
+
+#endif /* __OBJC__ */
+
+
+#endif
+
